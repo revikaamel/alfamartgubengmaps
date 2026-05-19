f94a36ad923b119415019b4e8cbe51f7dbf04e56
revikaamel <revikafeftyana@gmail.com>
2026-05-19 09:53:56 +0700

    Perbaikan rute tempat agar ada 2 opsi

--- lib/screens/route_page.dart
+++ lib/screens/route_page.dart
@@ -7,7 +7,6 @@ import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 
 class RoutePage extends StatefulWidget {
-
   final Map place;
   final LatLng currentLocation;
 
@@ -18,43 +17,34 @@ class RoutePage extends StatefulWidget {
   });
 
   @override
-  State<RoutePage> createState() =>
-      _RoutePageState();
+  State<RoutePage> createState() => _RoutePageState();
 }
 
-class _RoutePageState
-    extends State<RoutePage> {
-
+class _RoutePageState extends State<RoutePage> {
   String vehicle = "Motor";
 
   List<LatLng> routePoints = [];
-
   bool isLoading = true;
+
+  double currentZoom = 13;
 
   @override
   void initState() {
-
     super.initState();
-
     getRoute();
   }
 
   Future<void> getRoute() async {
-
     setState(() {
       isLoading = true;
     });
 
     try {
-
       String profile = "driving";
 
       if (vehicle == "Jalan Kaki") {
-
         profile = "foot";
-
       } else if (vehicle == "Sepeda") {
-
         profile = "cycling";
       }
 
@@ -67,24 +57,18 @@ class _RoutePageState
           "${widget.place['lat']}"
           "?overview=full&geometries=geojson";
 
-      final response =
-          await http.get(Uri.parse(url));
+      final response = await http.get(Uri.parse(url));
 
       if (response.statusCode == 200) {
-
-        final data =
-            jsonDecode(response.body);
+        final data = jsonDecode(response.body);
 
         final coordinates =
-            data['routes'][0]['geometry']
-                ['coordinates'];
+            data['routes'][0]['geometry']['coordinates'];
 
         List<LatLng> points = [];
 
         for (var point in coordinates) {
-
           points.add(
-
             LatLng(
               point[1],
               point[0],
@@ -93,64 +77,46 @@ class _RoutePageState
         }
 
         setState(() {
-
           routePoints = points;
-
           isLoading = false;
         });
       }
     } catch (e) {
-
       setState(() {
-
         isLoading = false;
       });
     }
   }
 
   double getDistance() {
-
     return Geolocator.distanceBetween(
-
       widget.currentLocation.latitude,
       widget.currentLocation.longitude,
-
       widget.place['lat'],
       widget.place['lng'],
     );
   }
 
   double getSpeed() {
-
     switch (vehicle) {
-
       case "Jalan Kaki":
         return 5;
-
       case "Sepeda":
         return 15;
-
       case "Mobil":
         return 30;
-
       default:
         return 40;
     }
   }
 
-  String getEstimatedTime(
-      double distanceKm) {
-
+  String getEstimatedTime(double distanceKm) {
     double speed = getSpeed();
+    double hours = distanceKm / speed;
 
-    double hours =
-        distanceKm / speed;
-
-    int minutes =
-        (hours * 60).round();
+    int minutes = (hours * 60).round();
 
     if (minutes <= 0) {
-
       minutes = 1;
     }
 
@@ -159,109 +125,73 @@ class _RoutePageState
 
   @override
   Widget build(BuildContext context) {
-
-    double distanceKm =
-        getDistance() / 1000;
-
-    String estimation =
-        getEstimatedTime(distanceKm);
+    double distanceKm = getDistance() / 1000;
+    String estimation = getEstimatedTime(distanceKm);
 
     return Scaffold(
-
       appBar: AppBar(
         title: const Text("Rute"),
       ),
-
       body: Column(
-
         children: [
-
           Expanded(
-
             child: FlutterMap(
-
               options: MapOptions(
-
-                initialCenter:
-                    widget.currentLocation,
-
+                initialCenter: widget.currentLocation,
                 initialZoom: 13,
+
+                onPositionChanged: (position, hasGesture) {
+                  setState(() {
+                    currentZoom = position.zoom ?? 13;
+                  });
+                },
               ),
-
               children: [
-
                 TileLayer(
-
                   urlTemplate:
                       'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                 ),
 
                 PolylineLayer(
-
                   polylines: [
-
                     Polyline(
-
-                      points: routePoints.isEmpty
-
+                      points: currentZoom < 14
                           ? [
-
                               widget.currentLocation,
-
                               LatLng(
                                 widget.place['lat'],
                                 widget.place['lng'],
                               ),
                             ]
-
                           : routePoints,
-
                       strokeWidth: 6,
-
                       color: Colors.red,
                     )
                   ],
                 ),
 
                 MarkerLayer(
-
                   markers: [
-
                     Marker(
-
-                      point:
-                          widget.currentLocation,
-
+                      point: widget.currentLocation,
                       width: 80,
                       height: 80,
-
                       child: const Icon(
-
                         Icons.my_location,
-
                         color: Colors.blue,
-
                         size: 40,
                       ),
                     ),
-
                     Marker(
-
                       point: LatLng(
-
                         widget.place['lat'],
                         widget.place['lng'],
                       ),
-
                       width: 80,
                       height: 80,
-
                       child: const Icon(
-
                         Icons.location_on,
-
                         color: Colors.red,
-
                         size: 40,
                       ),
                     ),
@@ -269,54 +199,34 @@ class _RoutePageState
                 ),
 
                 if (isLoading)
-
                   const Center(
-                    child:
-                        CircularProgressIndicator(),
+                    child: CircularProgressIndicator(),
                   ),
               ],
             ),
           ),
 
           Container(
-
-            padding:
-                const EdgeInsets.all(15),
-
+            padding: const EdgeInsets.all(15),
             child: Column(
-
-              crossAxisAlignment:
-                  CrossAxisAlignment.start,
-
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-
                 DropdownButton<String>(
-
                   value: vehicle,
-
                   isExpanded: true,
-
                   items: [
-
                     "Mobil",
                     "Motor",
                     "Sepeda",
                     "Jalan Kaki"
-
                   ].map((item) {
-
                     return DropdownMenuItem(
-
                       value: item,
-
                       child: Text(item),
                     );
                   }).toList(),
-
                   onChanged: (value) async {
-
                     setState(() {
-
                       vehicle = value!;
                     });
 
