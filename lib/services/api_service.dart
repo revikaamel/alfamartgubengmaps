23db5abc8b68d2861b9c6351a5a041e96b99c7b8
revikaamel <revikafeftyana@gmail.com>
2026-05-20 00:38:40 +0700

    Mencoba memperbaiki database

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 class ApiService {
 
   static const String apiUrl =
-      'https://script.google.com/macros/s/AKfycbyL1L2bECCq2oYH7bGBvaiPHl-qNGoHTQvQDVzk-D763yRc5mlwcoLRteENZUDattr7/exec';
+      'https://script.google.com/macros/s/AKfycbzD4A2FKHzxO0P_dGtvO_2tBZgDTVr-a-2QJPM_mo7O-yelIYuDGRN4tCVF7amokB53WA/exec';
 
   static Future<List<dynamic>>
       getPlaces() async {
